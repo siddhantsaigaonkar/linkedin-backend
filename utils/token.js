35e821b1ba78982fe57415644afb74d133282fb7
siddhantsaigaonkar <siddhantsaigaonkar14k@gmail.com>
2026-06-16 13:04:46 +0530

    make midification in token.js and return the token

--- utils/token.js
+++ utils/token.js
@@ -1,22 +1,3 @@
-// import jwt from "jsonwebtoken";
-
-// export const genToken = (userId) => {
-//   try {
-//     const token = jwt.sign(
-//       { userId },
-//       process.env.JWT_SECRET,
-//       {
-//         expiresIn: "7d",
-//       }
-//     );
-
-//     return token;
-//   } catch (error) {
-//     console.log("Token Generation Error:", error.message);
-//     throw error;
-//   }
-// };
-
 import jwt from "jsonwebtoken";
 
 export const genToken = async (userId) => {
